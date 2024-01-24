7c7f74798dc5a1154fcb4e66adb9935c29b366d6
Colum Kelly <columk1@users.noreply.github.com>
2024-01-24 00:45:11 -0800

    Add useMousePosition hook

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,11 +1,40 @@
 import styles from './Game.module.css'
+import { useRef, useEffect, useState } from 'react'
+import useMousePosition from '../../hooks/useMousePosition.js'
 
 const Game = () => {
+  const [size, setSize] = useState()
+  const imgRef = useRef(null)
+
+  const { x, y } = useMousePosition()
+
+  function handleMouseMove(event) {}
+
+  useEffect(() => {
+    const getSize = () => {
+      let width = imgRef.current.offsetWidth * 2
+      let height = imgRef.current.offsetHeight * 2
+      return `${width}px ${height}px`
+    }
+    setSize(getSize())
+  }, [imgRef])
+
   return (
     <>
       <div className={styles.imgContainer}></div>
-      <img src='elcap.jpg'></img>
+      <ZoomOverlay size={size} />
+      <img ref={imgRef} src='elcap.jpg' onMouseMove={handleMouseMove}></img>
     </>
+  )
+}
+
+const ZoomOverlay = (size, handleMouseMove) => {
+  return (
+    <div
+      onMouseMove={handleMouseMove}
+      className={styles.zoomOverlay}
+      style={{ backgroundSize: size }}
+    ></div>
   )
 }
 
