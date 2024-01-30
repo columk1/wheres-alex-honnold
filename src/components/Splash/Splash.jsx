3db7d10042c154a0b08bbf0e63fbc832b61421e9
Colum Kelly <columk1@users.noreply.github.com>
2024-01-29 21:51:41 -0800

    Add svg icons

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -7,13 +7,17 @@ export default function Splash({ startGame }) {
         <img className={styles.logo} alt='Logo' />
       </div>
       <h1>Where's Alex Honnold?</h1>
+      <p>
+        Your task is to find Alex Honnold and some of his favourite El Cap features in as little
+        time as possible.
+      </p>
 
       <div className={styles.startBtn}>
         <button className={styles.btn} onClick={startGame}>
           Start Game
         </button>
       </div>
-      <p className={styles.description}>A React project assignment from The Odin Project</p>
+      <p className={styles.description}>Find our hero on el Cap</p>
     </>
   )
 }
